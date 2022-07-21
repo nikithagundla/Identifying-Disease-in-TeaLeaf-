4391e2cc364fca86d4cc43c9ec2fd752a67fee91
Nikitha <109679260+nikithagundla@users.noreply.github.com>
2022-07-21 20:34:48 +0530

    Update script (1) (1).js

--- script (1) (1).js
+++ script (1) (1).js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:"https://www.nyckel.com/v1/functions/ivj8zz62hjq3yw9l/invoke",
+        url:"https://www.nyckel.com/v1/functions/zqeeot07egy24asg/invoke",
         type: 'post',
         data: formdata,
         contentType: false,
